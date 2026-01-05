ac781a31a82d35b704158150c35924dc6f58677a
KenAKAFrosty <kenakafrosty@gmail.com>
2026-01-04 18:15:00 -0600

    model 13, been growing every epoch so far

--- battle_server/src/main.rs
+++ battle_server/src/main.rs
@@ -442,7 +442,7 @@ async fn main() -> anyhow::Result<()> {
              
              // Load the record explicitly
              let record = NamedMpkFileRecorder::<FullPrecisionSettings>::new()
-                .load("model-8".into(), &device)
+                .load("model-13".into(), &device)
                 .expect("Failed to load transformer weights");
              
              // Init and load
