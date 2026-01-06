3e8fa478f75f438f9ecc93ec3e6f37a231879c4e
KenAKAFrosty <kenakafrosty@gmail.com>
2026-01-06 12:16:12 -0600

    mimicry-focused approach, should at LEAST just not kill itself

--- battle_server/src/main.rs
+++ battle_server/src/main.rs
@@ -31,7 +31,7 @@ impl ModelKind {
         match self {
             ModelKind::OriginalCnn => "#D34516",
             ModelKind::HydraTransformer => "#D34516",
-            ModelKind::OxTransformer => "#422C44",
+            ModelKind::OxTransformer => "#542F3E",
             
         }
     }
@@ -496,7 +496,7 @@ async fn main() -> anyhow::Result<()> {
              
              // Load the record explicitly
              let record = NamedMpkFileRecorder::<FullPrecisionSettings>::new()
-                .load("model-3".into(), &device)
+                .load("model-1".into(), &device)
                 .expect("Failed to load transformer weights");
              
              // Init and load
