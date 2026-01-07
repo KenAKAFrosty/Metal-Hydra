21b3823ab76ffc6add5b0315eb028603a0ef44f0
KenAKAFrosty <kenakafrosty@gmail.com>
2026-01-06 19:15:49 -0600

    adjusting point assignment on this pass, trying epoch 3. loss showing some of the funkier signs but looking to be a bit better. let's see

--- battle_server/src/main.rs
+++ battle_server/src/main.rs
@@ -496,7 +496,7 @@ async fn main() -> anyhow::Result<()> {
              
              // Load the record explicitly
              let record = NamedMpkFileRecorder::<FullPrecisionSettings>::new()
-                .load("model-4".into(), &device)
+                .load("model-3".into(), &device)
                 .expect("Failed to load transformer weights");
              
              // Init and load
