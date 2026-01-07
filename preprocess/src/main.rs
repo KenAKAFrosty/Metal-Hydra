f3d7d4cf48f477f7c2903caaec2cd58b5088111c
KenAKAFrosty <kenakafrosty@gmail.com>
2026-01-07 13:57:16 -0600

    adjust value assignment. first epoch

--- preprocess/src/main.rs
+++ preprocess/src/main.rs
@@ -151,13 +151,13 @@ fn main() {
 const LOSER_START_VAL: f32 = 0.0;
 const LOSER_END_VAL: f32 = -0.5;
 
-const WINNER_START_VAL: f32 = 0.20;
+const WINNER_START_VAL: f32 = 0.5;
 const WINNER_END_VAL: f32 = 1.0;
 
 const DEATH_PENALTY: f32 = -1.0;
 const UNTAKEN_VAL: f32 = 0.0;
 
-const TIME_EXPONENT: f32 = 3.0; // 1.0 linear, 2.0 quadratic, 3.0 cubic
+const TIME_EXPONENT: f32 = 4.0; // 1.0 linear, 2.0 quadratic, 3.0 cubic
 
 fn process_game_buffer(
     turns: &[TrainingExample],
