f2086f8d4f32107976fc9364dab976864dd8d6fd
KenAKAFrosty <kenakafrosty@gmail.com>
2026-01-08 13:50:49 -0600

    12

--- battle_server/src/main.rs
+++ battle_server/src/main.rs
@@ -496,7 +496,7 @@ async fn main() -> anyhow::Result<()> {
              
              // Load the record explicitly
              let record = NamedMpkFileRecorder::<FullPrecisionSettings>::new()
-                .load("model-7".into(), &device)
+                .load("model-12".into(), &device)
                 .expect("Failed to load transformer weights");
              
              // Init and load
